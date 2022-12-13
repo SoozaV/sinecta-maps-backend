d22fdab81d9b6c9592e065cb760a4a4b2918af3a
SoozaV <s8za.92@gmail.com>
2022-12-13 16:43:44 -0600

    Restart backend config

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
-import express from "express";
-import cors from 'cors'
-import routes from "./routes/polygons.routes"; 
- 
+import express, { Request, Response } from "express";
+import cors from "cors";
+import routes from "./routes/polygons.routes";
+
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
+
+app.get("/", (_req: Request, res: Response) => res.send("Express Typescript on Vercel"));
+app.get("/ping", (_req: Request, res: Response) => res.send("pong 🏓"));
+
 app.use("/api", routes);
 
 export default app;
-  
