bfd020c839507f25fd8839d02e9bee44b2550265
SoozaV <s8za.92@gmail.com>
2022-12-12 18:28:18 -0600

    Fixing db import src

---
+++ public/database/index.js
@@ -0,0 +1,4 @@
+"use strict";
+sequelize;
+from;
+"./database";
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import app from "./src/app";
-import sequelize from "./src/database/database";
+import app from "./app";
+import sequelize from "./database/database";
 
 const port = process.env.PORT || 3000;
 
