b8c121b25b2c4cc09f99ce634249ac96a73c699f
SoozaV <s8za.92@gmail.com>
2022-12-12 19:11:26 -0600

    fix index import

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-import app from "./app";
-import sequelize from "./database/database";
+import app from "./src/app";
+import sequelize from "./src/database/database";
 
 const port = process.env.PORT || 3000;
 
--- public/index.js
+++ public/index.js
@@ -12,8 +12,8 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("./app"));
-const database_1 = __importDefault(require("./database/database"));
+const app_1 = __importDefault(require("./src/app"));
+const database_1 = __importDefault(require("./src/database/database"));
 const port = process.env.PORT || 3000;
 function main() {
     return __awaiter(this, void 0, void 0, function* () {
